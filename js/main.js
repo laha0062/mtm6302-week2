2172092fe67445786c9231f7916f718b3437eb16
Iain Lahaie <laha0062@algonquinlive.com>
2023-05-19 14:02:54 -0400

    adds demo for variables

--- js/main.js
+++ js/main.js
@@ -1,1 +1,33 @@
-console.log("Js file included")
+// console.log("Js file included")
+/*
+the above line of code is commented out using a single line comment, the shortcut is control + /
+
+this comment is a multiline comment, the shortcut is shift + alt + A
+*/
+
+let myName = "Iain"
+console.log(myName)
+myName = "Iain Lahaie"
+console.log(myName)
+
+const address = "Earth"
+console.log(address)
+
+let myProfession
+myProfession = 
+console.log(myProfession)
+
+//array is an object data type
+const myScore = [56,76,98]
+console.log(myScore)
+
+let num = 10
+num += 2
+console.log(num)
+
+console.log(num === "12")
+// the && logical operator will be true if all conditions are met
+console.log(num >= 7 && num !=="12")
+// the ! means not
+//the || logical operator will be true if any of the conditions are met
+console.log(num > 7 || num !==12)
