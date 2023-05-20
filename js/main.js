9176a62f403ce93d691982d6657f289536ecd06b
Iain Lahaie <laha0062@algonquinlive.com>
2023-05-20 10:50:44 -0400

    completes in class demo

--- js/main.js
+++ js/main.js
@@ -31,3 +31,16 @@ console.log(num >= 7 && num !=="12")
 // the ! means not
 //the || logical operator will be true if any of the conditions are met
 console.log(num > 7 || num !==12)
+
+numA = "1"
+numB = 1
+console.log(numA + numB)
+//parseInt takes string and converts it into an number
+console.log(parseInt(numA) + numB)
+
+const pets = 2
+const pet1Name = "Simba"
+const pet2Name = "Bufi"
+
+console.log("I have " + pets + " dogs, " + pet1Name + " & " + pet2Name)
+console.log(`I have ${pets} dogs, ${pet1Name} & ${pet2Name}`)//using template literals
